7b7f0842bc40c2a3af598335b0a76b823587fb55
biryukov <biryukovka99@gmail.com>
2018-07-10 13:15:31 +0600

    Well that's confusing

--- calc/Yikes!/Yikes!/Form1.cs
+++ calc/Yikes!/Yikes!/Form1.cs
@@ -18,13 +18,17 @@ namespace Yikes_
 
         private void rnd_Click(object sender, EventArgs e)
         {
+            MultiplyCalculator mult;
+            DivisionCalculator div;
+            AdditionCalculator adder;
+            SubstractionCalculator subber;
             double currnum1 = Convert.ToDouble(textBox1.Text), currnum2 = Convert.ToDouble(textBox2.Text), endres;
             switch (((Button)sender).Name)
             {
-                case ("bplus"): endres = currnum1 + currnum2; break;
-                case ("bdiv"): endres = currnum1 / currnum2;  break;
-                case ("bminus"): endres = currnum1 - currnum2; break;
-                case ("bmult"): endres = currnum1 * currnum2;  break;
+                case ("bplus"): endres = adder.CalcMet(currnum1, currnum2); break;
+                case ("bdiv"): endres = div.CalcMet(currnum1, currnum2);  break;
+                case ("bminus"): endres = subber.CalcMet(currnum1, currnum2); break;
+                case ("bmult"): endres = mult.CalcMet(currnum1, currnum2);  break;
                 default: throw new Exception("Неизвестная операция");
             }
             label1.Text = Convert.ToString(endres);
